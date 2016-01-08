e646c1fb8cfd257489ea634a7656cf6dd132bb6f
Tal Bortman <talbor49@gmail.com>
2016-01-08 18:21:44 +0200

    Added solution to 76

---
+++ src/main/java/com/solutions/Solution76.java
@@ -0,0 +1,21 @@
+package com.solutions;
+
+import java.util.*;
+
+public class Solution76 {
+
+    public final int LIMIT = 100;
+
+    public static long[] waysToWrite = new long[LIMIT+1];
+
+    public static void main(String[] args) {
+        waysToWrite[0] = 1;
+        for(int i = 1;i < LIMIT; i++) {
+            for(int j = i; j < waysToWrite.length; j++) {
+                waysToWrite[j] += waysToWrite[j-i];
+            }
+        }
+        System.out.println(waysToWrite[100]);
+    }
+
+}
